3e8a31e8d560fa0f61ae0dd3ff1e226844de451d
Nicholas Fung <nbfung@sfu.ca>
2017-11-17 11:42:34 -0800

    Add Item class with appropriate Enums

---
+++ ItsFree/Enums.swift
@@ -0,0 +1,33 @@
+//
+//  Enums.swift
+//  ItsFree
+//
+//  Created by Nicholas Fung on 2017-11-17.
+//  Copyright © 2017 Sanjay Shah. All rights reserved.
+// 
+
+import Foundation
+
+public enum ItemCategory {
+    case Books,
+    Clothing,
+    Furniture,
+    ArtAndCollectables,
+    SportingGoods,
+    Electronics,
+    HomeAppliances,
+    JewelleryAndWatches,
+    Toys,
+    BuildingToolsAndSupplies,
+    IndoorDecor,
+    OutdoorDecor,
+    Other
+}
+
+public enum ItemQuality {
+    case New,
+    GentlyUsed,
+    WellUsed,
+    DamagedButFunctional,
+    NeedsFixing
+}

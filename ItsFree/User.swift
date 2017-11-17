8d9446860ec2bf92a879f183dac9a8e2ed3a7911
Nicholas Fung <nbfung@sfu.ca>
2017-11-17 13:26:12 -0800

    Add User Model

---
+++ ItsFree/User.swift
@@ -0,0 +1,30 @@
+//
+//  User.swift
+//  ItsFree
+//
+//  Created by Nicholas Fung on 2017-11-17.
+//  Copyright © 2017 Sanjay Shah. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    var UID:String!
+    var email:String
+    var name:String
+    var rating:Int
+    
+    
+    init(with email:String, name:String, and rating:Int) {
+        self.email = email
+        self.name = name
+        self.rating = rating
+    }
+    
+    
+    
+    
+    
+    
+    
+}

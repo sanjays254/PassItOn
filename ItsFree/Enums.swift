7504bd11b6ad05e89cdde88b51b4bd206fe344f1
Sanjay Shah <Sanjay Shah>
2017-11-17 17:40:20 -0800

    added category table view

--- ItsFree/Enums.swift
+++ ItsFree/Enums.swift
@@ -9,19 +9,60 @@
 import Foundation
 
 public enum ItemCategory:String {
-    case Books = "Books",
-    Clothing = "Clothing",
-    Furniture = "Furniture",
-    ArtAndCollectables = "Art & Collectables",
-    SportingGoods = "Sporting Goods",
-    Electronics = "Electronics",
-    HomeAppliances = "Home Appliances",
-    JewelleryAndWatches = "Jewellery & Watches",
-    Toys = "Toys",
-    BuildingToolsAndSupplies = "Building Tools & Supplies",
-    IndoorDecor = "Indoor Decor",
-    OutdoorDecor = "Outdoor Decor",
-    Other = "Other"
+    
+    
+    case books = "Books",
+    clothing = "Clothing",
+    furniture = "Furniture",
+    artAndCollectables = "Art & Collectables",
+    sportingGoods = "Sporting Goods",
+    electronics = "Electronics",
+    homeAppliances = "Home Appliances",
+    jewelleryAndWatches = "Jewellery & Watches",
+    toys = "Toys",
+    buildingToolsAndSupplies = "Building Tools & Supplies",
+    indoorDecor = "Indoor Decor",
+    outdoorDecor = "Outdoor Decor",
+    other = "Other"
+    
+    static func stringValue(index: Int) -> String {
+        switch index {
+        case 0:
+            return ItemCategory.books.rawValue
+        case 1:
+            return ItemCategory.clothing.rawValue
+        case 2:
+            return ItemCategory.furniture.rawValue
+        case 3:
+            return ItemCategory.artAndCollectables.rawValue
+        case 4:
+            return ItemCategory.sportingGoods.rawValue
+        case 5:
+            return ItemCategory.electronics.rawValue
+        case 6:
+            return ItemCategory.homeAppliances.rawValue
+        case 7:
+            return ItemCategory.jewelleryAndWatches.rawValue
+        case 8:
+            return ItemCategory.toys.rawValue
+        case 9:
+            return ItemCategory.buildingToolsAndSupplies.rawValue
+        case 10:
+            return ItemCategory.indoorDecor.rawValue
+        case 11:
+            return ItemCategory.outdoorDecor.rawValue
+        case 12:
+            return ItemCategory.other.rawValue
+        default:
+            return "Error"
+        }
+        
+    }
+    //gets count of categories
+    static var count: Int { return ItemCategory.other.hashValue + 1
+        
+    }
+    
 }
 
 public enum ItemQuality:String {
--- ItsFree/PostViewController.swift
+++ ItsFree/PostViewController.swift
@@ -9,28 +9,42 @@
 import UIKit
 import MapKit
 
-class PostViewController: UIViewController, MKMapViewDelegate, UITextFieldDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
+class PostViewController: UIViewController, MKMapViewDelegate, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
+
     
 
+    var categoryCount: Int!
     
     
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var descriptionTextField: UITextField!
-    @IBOutlet weak var categoryPicker: UIPickerView!
-    var categoryPickerData: [String] = [String]()
     @IBOutlet weak var qualitySegmentedControl: UISegmentedControl!
     @IBOutlet weak var tagButtonView: UIView!
     @IBOutlet weak var itemMapView: MKMapView!
     
     
+    @IBOutlet weak var addCategoryButton: UIButton!
+    
+    var categoryTableView: UITableView!
+    
+    let cellID: String = "categoryCellID"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         titleTextField.delegate = self
         descriptionTextField.delegate = self
+        descriptionTextField.borderStyle = UITextBorderStyle.roundedRect
+        
+        
+        
+        categoryTableView = UITableView(frame: CGRect(x: 20, y:20, width: 250, height: 500), style: UITableViewStyle.plain)
+        
+        
+        categoryTableView.delegate = self
+        categoryTableView.dataSource = self
         
 
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -44,13 +58,55 @@ class PostViewController: UIViewController, MKMapViewDelegate, UITextFieldDelega
         
     }
     
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
+    
+    @IBAction func openCategories(_ sender: UIButton) {
+        
+
+        self.view.addSubview(categoryTableView)
+        
+        categoryTableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
+        
+        self.view.bringSubview(toFront: categoryTableView)
+        
+
+        
     }
     
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return 5
+    
+    
+    
+    
+    //category chooser table views
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return ItemCategory.count
+        
+        
     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = categoryTableView.dequeueReusableCell(withIdentifier: cellID)!
+        
+        cell.textLabel?.text = ItemCategory.stringValue(index: indexPath.row)
+        
+        
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let cell = tableView.cellForRow(at: indexPath)!
+        
+        self.addCategoryButton.setTitle("Category: \(cell.textLabel?.text ?? "Unknown")", for: UIControlState.normal)
+        
+        categoryTableView.removeFromSuperview()
+    }
+    
+    
     
     
     

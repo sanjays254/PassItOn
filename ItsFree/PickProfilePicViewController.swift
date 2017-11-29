a96abfc907683c0c09b1dd3e2cc60e73b450393d
Nicholas Fung <nbfung@sfu.ca>
2017-11-29 15:01:47 -0800

    Add screen to choose a profile pic after login if the user wants to

---
+++ ItsFree/PickProfilePicViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PickProfilePicViewController.swift
+//  ItsFree
+//
+//  Created by Nicholas Fung on 2017-11-29.
+//  Copyright © 2017 Sanjay Shah. All rights reserved.
+//
+
+import UIKit
+
+class PickProfilePicViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
